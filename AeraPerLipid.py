dac696998d2625bb677ee44ae1909f766a8d23e0
Moutoussamy <57101711+Moutoussamy@users.noreply.github.com>
2020-05-22 11:12:46 +0200

    Initial commit

---
+++ AeraPerLipid.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+Ce programme permet de calculer le rmsf ainsi que le B-factor.
+"""
+
+__author__ = "Emmanuel Edouard MOUTOUSSAMY"
+__version__  = "1.0.0"
+__copyright__ = "copyleft"
+__date__ = "2014/11"
+
+import sys
+import os
+import numpy as np
+import matplotlib.pyplot as plt
